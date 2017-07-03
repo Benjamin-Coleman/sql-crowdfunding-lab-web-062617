303d433a293e1ce723e2266d223a9a174dadf743
Benjamin-Coleman <github email address>
2017-07-03 16:56:15 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,23 @@
+
+-- title, a category, a funding goal, a start date, and an end date.
+CREATE TABLE projects(
+	id INTEGER PRIMARY KEY,
+	title TEXT,
+	category TEXT,
+	funding_goal REAL,
+	start_date TEXT,
+	end_date TEXT
+);
+
+CREATE TABLE users(
+	id INTEGER PRIMARY KEY,
+	name TEXT,
+	age INTEGER
+);
+
+CREATE TABLE pledges(
+	id INTEGER PRIMARY KEY,
+	amount INTEGER,
+	user_id INTEGER,
+	project_id INTEGER
+);
